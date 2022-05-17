176e379eb06267631b8f1890381f34ec795170f3
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-18 00:26:06 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        pop:"'Poppins',serif",
-        spc:"'Space Grotesk',serif"
-      }
+      fontFamily: {
+        pop: "'Poppins',serif",
+        spc: "'Space Grotesk',serif",
+      },
     },
   },
   plugins: [],
-}
+};
